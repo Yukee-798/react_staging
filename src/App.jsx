f791fb70e02a34d34714e8cf46a40715db5587ca
Yukee-798 <909397116@qq.com>
2020-12-25 13:14:57 +0800

    TodoList案例_组件静态化

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
+import Header from './components/Header';
+import List from './components/List';
+import Footer from './components/Footer';
+
+import './App.css';
+
+
 class App extends Component {
     render() {
         return (
             <div>
-                
+                <div className="todo-container">
+                    <div className="todo-wrap">
+                        <Header />
+                        <List />
+                        <Footer />
+                   
+                    </div>
+                </div>
             </div>
         );
     }
---
+++ src/components/List/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Item from '../Item';
+import './index.css';
+
+class List extends Component {
+    render() {
+        return (
+            <ul className="todo-main">
+                <Item />
+                <Item />
+                <Item />
+            </ul>
+        );
+    }
+}
+
+export default List;
