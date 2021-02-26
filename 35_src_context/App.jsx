34c30973ed589e8f18767f90775526e29ba04888
Yukee-798 <909397116@qq.com>
2021-02-26 19:28:44 +0800

    context

---
+++ 35_src_context/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import A from './components/A'
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <A />
+            </div>
+        );
+    }
+}
+
+export default App;
